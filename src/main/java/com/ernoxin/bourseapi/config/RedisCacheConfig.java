3f688280a21cc6096f305aaf4896644049cb0e92
ErfanDavoodiNasr <davoodinasr.erfan@gmail.com>
2026-06-20 23:22:05 +0330

    fix: use default Redis JSON serializer for cache entries

--- src/main/java/com/ernoxin/bourseapi/config/RedisCacheConfig.java
+++ src/main/java/com/ernoxin/bourseapi/config/RedisCacheConfig.java
@@ -1,8 +1,5 @@
 package com.ernoxin.bourseapi.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -25,18 +22,8 @@ public class RedisCacheConfig {
 
     @Bean
     @ConditionalOnProperty(name = "cache.enabled", havingValue = "true", matchIfMissing = true)
-    GenericJackson2JsonRedisSerializer redisCacheSerializer(ObjectMapper objectMapper) {
-        ObjectMapper cacheObjectMapper = objectMapper.copy();
-        cacheObjectMapper.activateDefaultTyping(
-                BasicPolymorphicTypeValidator.builder()
-                        .allowIfSubType("com.ernoxin.bourseapi")
-                        .allowIfSubType("com.fasterxml.jackson.databind")
-                        .allowIfSubType("java.util")
-                        .build(),
-                ObjectMapper.DefaultTyping.EVERYTHING,
-                JsonTypeInfo.As.PROPERTY
-        );
-        return new GenericJackson2JsonRedisSerializer(cacheObjectMapper);
+    GenericJackson2JsonRedisSerializer redisCacheSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
     }
 
     @Bean
